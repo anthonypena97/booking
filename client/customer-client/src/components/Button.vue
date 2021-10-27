10aa5b1021e7982f6e135f4a36ebf195a832d0dc
patrick <patrickxandrews@gmail.com>
2021-10-27 14:35:57 -0600

    overwrite

---
+++ client/customer-client/src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="btn">
+      <button @click="sendBack()">{{ text }}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+      text: String
+  },
+  methods: {
+      sendBack() {
+          this.$emit("something");
+      }
+  }
+}
+</script>
+
+<style scoped>
+#btn {
+  width: 6000px;
+}
+</style>
